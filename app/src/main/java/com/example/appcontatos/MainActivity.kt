989d8134e0f0aa5a9a6bbb85258ddaaaa9acdc5b
lWillian <willian.vlcs@gmail.com>
2021-08-25 22:15:48 -0300

    Criação de itens de menu e menu de navegação

--- app/src/main/java/com/example/appcontatos/MainActivity.kt
+++ app/src/main/java/com/example/appcontatos/MainActivity.kt
@@ -5,9 +5,13 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     private val rvList: RecyclerView by lazy {
@@ -17,9 +21,20 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.drawer_menu)
+        initDrawer()
         bindViews()
         updateList()
+    }
+
+    private fun initDrawer(){
+        val drawerLayout = findViewById<View>(R.id.drawer_layout) as DrawerLayout
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar,R.string.open_drawer, R.string.close_drawer)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
     }
 
     private fun bindViews() {
